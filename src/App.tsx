8a1d48c08a5b95e1acd720f06ecc746fe25ded52
bliss-armah <blissarmah8@gmail.com>
2023-05-30 06:51:28 +0000

    changes amd code refactored

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import { Footer } from "./components";
 import { HomePage, ProductPage, AboutPage, ContactPage } from "./pages";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   return (
@@ -14,6 +16,10 @@ function App() {
         </Routes>
       </BrowserRouter>
       <Footer />
+      <ToastContainer
+        theme="colored"
+        toastStyle={{ backgroundColor: "#f7d06e" }}
+      />
     </>
   );
 }
--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,34 +1,40 @@
-// import React from 'react'
-// import { FormData } from "../Schema/Form";
+import React from "react";
 
-// interface FormFieldProps {
-//     label: string;
-//     type: string;
-//     name: keyof FormData;
-//     value: string ;
-//     error: string;
-//     onChange: (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => void;
-//   }
+interface InputFieldProps {
+  label: string;
+  id: string;
+  name: string;
+  value: string;
+  error: string;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
 
-// const FormField : React.FC<FormFieldProps> = ({label,type,name,value,onChange,error}) => {
-//   return (
-//     <div>
-//       <label htmlFor={name} className="block mb-1">
-//         {label}
-//       </label>
-//       <input
-//         type={type}
-//         id={name}
-//         name={name}
-//         value={value}
-//         onChange={onChange}
-//         className={`w-full px-4 py-2 border rounded-md focus:outline-none  ${
-//           error ? "border-red-500" : "focus:ring-[#e9b949]"
-//         }`}
-//       />
-//       {error && <p className="text-red-500 text-sm mt-1">{error}</p>}
-//     </div>
-//   )
-// }
+const InputField: React.FC<InputFieldProps> = ({
+  label,
+  id,
+  name,
+  value,
+  error,
+  onChange,
+}) => {
+  return (
+    <div>
+      <label htmlFor={id} className="block mb-1 text-[1.1rem] text-[#617d98]">
+        {label}
+      </label>
+      <input
+        type="text"
+        id={id}
+        name={name}
+        value={value}
+        onChange={onChange}
+        className={`w-full px-4 py-2 border rounded-md focus:outline-none ${
+          error ? "border-red-500" : "focus:ring-[#e9b949]"
+        }`}
+      />
+      {error && <p className="text-red-500 text-sm mt-1">{error}</p>}
+    </div>
+  );
+};
 
-// export default FormField
+export default InputField;
